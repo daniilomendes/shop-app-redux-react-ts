93e0ddb111b5ff27af1deca514d555865b34cdfc
Danilo Mendes <danilo.mirandha@gmail.com>
2024-04-13 17:50:43 -0300

    feat: add setup redux store hook

---
+++ src/store/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { cartSlice } from "./cart-slice";
+
+export const store = configureStore({
+  reducer: {
+    cart: cartSlice.reducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
