09c70d26d96b081c86db44aad4cb0dcb1a062007
Danilo Mendes <danilo.mirandha@gmail.com>
2024-04-13 17:48:26 -0300

    feat: add shop product items

---
+++ src/dummy-products.ts
@@ -0,0 +1,58 @@
+import mochaOvercoat from "./assets/mocha-overcoat.jpg";
+import dreamGown from "./assets/dream-gown.jpg";
+import rainJacket from "./assets/rain-jacket.jpg";
+import merlotSuit from "./assets/merlot-suit.jpg";
+import moonlightDress from "./assets/moonlight-dress.jpg";
+import denimPioneer from "./assets/denim-pioneer.jpg";
+
+export const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    image: mochaOvercoat,
+    title: "Sobretudo Mocha Majestoso Vintage",
+    price: 129.99,
+    description:
+      "Canalize a sofisticação atemporal com este deslumbrante sobretudo mocha. Feito para o cavalheiro exigente que aprecia a mistura fina de charme vintage e elegância moderna.",
+  },
+  {
+    id: "p2",
+    image: dreamGown,
+    title: "Vestido de sonho blush encantador",
+    price: 189.99,
+    description:
+      "Aproveite o brilho da elegância com nosso vestido Enchanting Blush Dream. Incorpore a realeza de uma rainha com um doce toque de rosa que sussurra encantamento. Esta é a peça perfeita para quem busca criar momentos inesquecíveis.",
+  },
+
+  {
+    id: "p3",
+    image: rainJacket,
+    title: "Jaqueta de chuva vintage Sunshine",
+    price: 49.99,
+    description:
+      "Prepare-se para o banho com estilo! Nossa jaqueta de chuva Vintage Sunshine garante que você se destaque, mesmo no clima mais sombrio. Porque a chuva nunca é uma razão para comprometer seu quociente de moda.",
+  },
+  {
+    id: "p4",
+    image: merlotSuit,
+    title: "Terno clássico Merlot",
+    price: 249.99,
+    description:
+      "Entre na sala de reuniões com confiança incomparável em nosso Classic Merlot Business Suit. Exalando um ar de classe refinada e poder discreto, é ideal para o executivo moderno que valoriza tradição e elegância.",
+  },
+  {
+    id: "p5",
+    image: moonlightDress,
+    title: "Vestido de noite etéreo ao luar",
+    price: 159.99,
+    description:
+      "Vira o ambiente com nosso vestido de noite Ethereal Moonlight. Feito para imitar o fascínio do luar, este vestido é uma homenagem para aqueles que apreciam o glamour sutil e uma silhueta marcante.",
+  },
+  {
+    id: "p6",
+    image: denimPioneer,
+    title: "Jaqueta jeans robusta Pioneer",
+    price: 79.99,
+    description:
+      "Nossa jaqueta jeans Pioneer Rugged é uma homenagem àqueles que personificam o espírito de aventura. Projetada com durabilidade e conforto em mente, esta jaqueta é um guarda-roupa essencial para o explorador urbano.",
+  },
+];
